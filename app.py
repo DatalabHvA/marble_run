735f46c2dddf3aa7819ede70b1a05693381e60f6
Pieter Bons <p.c.bons@hva.nl>
2024-10-07 17:21:44 +0200

    all in Dutch and add direct feedback to retraining model

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ model_rf = joblib.load('marble_run_model_rf.pkl')
 data = pd.read_csv('marble_run_data.csv')
 
 # Streamlit app header
-st.title("Marble Run Experiment Demo")
+st.header("Knikkerbaan machine learning demo")
 
 # User input for parameters
 param1 = st.slider("# rechte bruggen", min_value=0, max_value=10, step=1)
@@ -53,20 +53,25 @@ param4 = st.selectbox("type knikker", ('A','B'))
 
 # Prediction
 prediction_lm, prediction_rf = get_predictions(model_lm, model_rf, param1, param2, param3, param4)
-st.write(f"Predicted Time linear model: {prediction_lm:.2f} seconds")
-st.write(f"Predicted Time random forest: {prediction_rf:.2f} seconds")
+placeholder = st.empty()
+placeholder2 = st.empty()
+placeholder.write(f"Voorspelde tijd linear model: {prediction_lm:.2f} seconden")
+placeholder2.write(f"Voorspelde tijd random forest: {prediction_rf:.2f} seconden")
 
 # User input for actual time
-actual_time = st.number_input("Actual Time (seconds)")
+actual_time = st.number_input("Echte tijd (seconden)")
 
 # Update the model with new data
-if st.button("Update Model"):
+if st.button("Update het model"):
 	model_lm, model_rf, data = update_models(model_lm, model_rf, data, param1, param2, param3, param4, actual_time)
-	# Save the updated model
-	st.success("Model updated successfully!")
+	prediction_lm, prediction_rf = get_predictions(model_lm, model_rf, param1, param2, param3, param4)# Save the updated model
+	placeholder.write(f"Predicted Time linear model: {prediction_lm:.2f} seconds")
+	placeholder2.write(f"Predicted Time random forest: {prediction_rf:.2f} seconds")
+
+	st.success(f"Model is succesvol geupdate op basis van {len(data)} data punten!")
 
 # Display predictions vs. parameters
-st.subheader("Predicted Time vs. Parameters")
+st.subheader("Voorspelde tijd vs. aantal rechte bruggen")
 
 data_pred = pd.DataFrame({'recht' : range(0, data['recht'].max()+1,1),'gebogen' : np.floor(data['gebogen'].mean()),'pijpje' : np.floor(data['pijpje'].mean()),'knikker_encoded' : data['knikker_encoded'].max()})
 data_pred['pred_lm'] = model_lm.predict(data_pred[['recht','gebogen','pijpje', 'knikker_encoded']])
